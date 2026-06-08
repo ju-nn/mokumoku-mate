8d2257c5ed9dc35250d9ec024295fb44e216c4d5
jun <rebirth0122@yahoo.co.jp>
2026-06-09 00:00:06 +0900

    Use local base in dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: "/mokumoku-mate/",
-});
+  base: command === "serve" ? "/" : "/mokumoku-mate/",
+}));
